0b3afd3e7baa9902dc15b7078c38af215370e0d8
Danny Willems <contact@danny-willems.be>
2016-05-22 02:10:31 +0200

    New config with package and Makefile separation.

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,7 +1,7 @@
 let on_device_ready _ =
   let c = Cordova_clipboard.t () in
-  c#copy "Hello world";
-  c#paste (fun str -> Dom_html.window##(alert (Js.string str)));
+  c#copy "Hello world" ();
+  c#paste (fun str -> Dom_html.window##(alert (Js.string str))) ();
   Js._false
 
 let _ =
