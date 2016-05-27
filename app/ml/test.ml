31e9143e08437f2592586d745adafa91f60ab05a
Danny Willems <contact@danny-willems.be>
2016-05-27 21:38:00 +0200

    update

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,7 +1,7 @@
 let on_device_ready _ =
-  let c = Cordova_clipboard.clipboard () in
-  Cordova_clipboard.copy c "Hello world" ();
-  Cordova_clipboard.paste c (fun str -> Jsoo_lib.alert str) ()
+  let c = Cordova_clipboard.t () in
+  c#copy "Hello world" ();
+  c#paste (fun str -> Jsoo_lib.alert str) ()
 
 let _ =
   Cordova.Event.device_ready on_device_ready
