4c992019beb5128337a39d22a155e0f5a31db09d
Danny Willems <contact@danny-willems.be>
2016-06-17 12:02:02 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,7 +1,6 @@
 let on_device_ready _ =
-  let c = Cordova_clipboard.t () in
-  c#copy "Hello world" ();
-  c#paste (fun str -> Jsoo_lib.alert str) ()
+  Cordova_clipboard.copy "Hello world 2" ();
+  Cordova_clipboard.paste (fun str -> Jsoo_lib.alert str) ()
 
 let _ =
   Cordova.Event.device_ready on_device_ready
